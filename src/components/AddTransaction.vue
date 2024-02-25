00b6ad77a9e7bcc5eb76f248b19513109bae61ee
Nick Rabone <n.rabone72@aol.com>
2024-02-25 23:23:03 +0000

    First commit

---
+++ src/components/AddTransaction.vue
@@ -0,0 +1,19 @@
+<template>
+    <h3>Add new transaction</h3>
+    <form id="form">
+        <div class="form-control">
+            <label for="text">Transaction</label>
+            <input type="text" id="text" placeholder="Enter details">
+        </div>
+        <div class="form-control">
+            <label for="amount">
+                Cost 
+                <br />
+                <span class="pos"> Income + </span>
+                <span class="neg"> Expense - </span>
+                </label>
+            <input type="text" id="amount" placeholder="Enter amount">
+        </div>
+        <button class="btn">Add Transaction</button>
+    </form>
+</template>
---
+++ src/components/Balance.vue
@@ -0,0 +1,4 @@
+<template>
+    <h4>Your Balances</h4>
+    <h1 id="balance">£0.00</h1>
+</template>
