363fe44349dd222d8cac085af5f95bd53db7573d
Nick Rabone <n.rabone72@aol.com>
2024-02-26 03:43:12 +0000

    7th commit - income and expenses working and toastification included

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <Header />
   <div class="container">
-    <Balance :total="total"/>
-    <IncomeExpenses />
+    <Balance :total="+total"/>
+    <IncomeExpenses :income="+income" :expenses="+expenses" />
     <TransactionList :transactions="transactions" />
     <AddTransaction />
   </div>
@@ -23,12 +23,29 @@
     { id: 2, text: 'Wages', amount: 1532.00 },
     { id: 3, text: 'Pet Plan', amount: -10.00 },
     { id: 4, text: 'Udemy', amount: -69.90 },
+    { id: 5, text: 'Rent', amount: -380.00 },
   ])
 
   const total = computed(() => {
     return transactions.value.reduce((acc, transaction) => {
       return acc + transaction.amount
-    }, 0)  
+    }, 0)
+    .toFixed(2)  
   })
-
+  const income = computed(() => {
+    return transactions.value
+      .filter((transaction) => transaction.amount > 0)
+      .reduce((acc, transaction) => {
+        return acc + transaction.amount
+      }, 0)
+      .toFixed(2)  
+  })
+  const expenses = computed(() => {
+    return transactions.value
+      .filter((transaction) => transaction.amount < 0)
+      .reduce((acc, transaction) => {
+        return acc + transaction.amount
+      }, 0)
+      .toFixed(2)  
+  })
 </script>
--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -1,9 +1,9 @@
 <template>
     <h3>Add new transaction</h3>
-    <form id="form">
+    <form id="form" @submit.prevent="onSubmit">
         <div class="form-control">
             <label for="text">Transaction</label>
-            <input type="text" id="text" placeholder="Enter details">
+            <input type="text" id="text" v-model="text" placeholder="Enter details">
         </div>
         <div class="form-control">
             <label for="amount">
@@ -12,9 +12,25 @@
                 <span class="pos"> Income + </span>
                 <span class="neg"> Expense - </span>
                 </label>
-            <input type="text" id="amount" placeholder="Enter amount">
+            <input type="text" id="amount" v-model="amount" placeholder="Enter amount">
         </div>
         <button class="btn">Add Transaction</button>
     </form>
 </template>
+
+<script setup>
+    import { ref } from 'vue'
+    import { useToast } from 'vue-toastification'
+
+    const text = ref('')
+    const amount = ref('')
+    const toast = useToast()
+
+    const onSubmit = () => {
+        if (!text.value || !amount.value) {
+            toast.error('Require Transaction and Cost ')
+        }
+        console.log(text.value, amount.value)    
+    }
+</script>
 
