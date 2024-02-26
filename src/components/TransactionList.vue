eedf2fc0cd5c72b5516feee0198765b962e22020
Nick Rabone <n.rabone72@aol.com>
2024-02-26 00:17:24 +0000

    2nd commit - using Options API in TransactionList.vue

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,11 +1,31 @@
 <template>
     <h3>History</h3>
     <ul id="list" class="list">
-        <li class="minus">
-            Cash <span>-£400</span><button class="delete-btn">x</button>
-        </li>
-        <li class="plus">
-            Wages <span>+£700</span><button class="delete-btn">x</button>
+        <li 
+            v-for="transaction in transactions"
+            :key="transaction.id"
+            :class="transaction.amount < 0 ? 'minus' : 'plus '"
+        >
+            {{ transaction.text }}<span>£{{ transaction.amount }}
+            </span>
+            <button class="delete-btn">x</button>
         </li>
     </ul>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                transactions: [
+                    { id: 1, text: 'Bus', amount: -32.00 },
+                    { id: 2, text: 'Wages', amount: 1532.00 },
+                    { id: 3, text: 'Pet Plan', amount: -10.00 },
+                    { id: 4, text: 'Udemy', amount: -69.90 },
+                ]
+            }    
+        }
+    }
+
+
+</script>
